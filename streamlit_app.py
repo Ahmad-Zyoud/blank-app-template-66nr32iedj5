02608ab86354c8e2c94d3171eb047e847fa1e19e
Ahmad-Zyoud <ahmadzyoud2000@gmail.com>
2024-07-08 15:50:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 num1 = st.slider("Number 1?", 0, 130, 25)
 num2 = st.slider("Number 2?", 0, 130, 25)
-st.write("Sum", sum(num1, num2))
+sum = num1 + num2
+st.write("Sum", sum)
