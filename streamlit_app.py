2c46e9a02c29431744b54ea7d0c9c445d9a4c6a7
Ahmad-Zyoud <ahmadzyoud2000@gmail.com>
2024-07-08 15:44:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
+my_data_element = st.line_chart(df)
+
+for tick in range(10):
+    time.sleep(.5)
+    add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
+    my_data_element.add_rows(add_df)
+
+st.button("Regenerate")
