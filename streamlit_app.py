34ad722a16e4f9b94db71d942cbad5789fd87297
Ahmad-Zyoud <ahmadzyoud2000@gmail.com>
2024-07-08 15:49:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import time
 
-df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
-my_data_element = st.line_chart(df)
-
-for tick in range(10):
-    time.sleep(.5)
-    add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
-    my_data_element.add_rows(add_df)
-
-st.button("Regenerate")
+num1 = st.slider("Number 1?", 0, 130, 25)
+num2 = st.slider("Number 2?", 0, 130, 25)
+st.write("Sum", sum(num1, num2))
